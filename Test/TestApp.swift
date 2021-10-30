911ae79bbbbb8cd1ffdd345a48124d69fabcb3c7
Jonathan Hogue <jonathan.hogue@live.com>
2021-10-30 18:50:38 -0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Jonathan Hogue on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
